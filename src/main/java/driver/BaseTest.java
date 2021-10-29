1d0bf689c98c1d30c663d07fcac174445963b1f9
Dzmitry <bogdanov111222@yandex.ru>
2021-10-29 13:52:52 +0300

    implemented driver logic using singleton pattern

---
+++ src/main/java/driver/BaseTest.java
@@ -0,0 +1,25 @@
+package driver;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
+import testNG.Listener;
+
+import static driver.DriverCreation.getDriver;
+import static driver.DriverCreation.quitDriver;
+
+@Listeners(Listener.class)
+public class BaseTest {
+    public WebDriver driver;
+
+    @BeforeTest
+    public void setupDriver() {
+        driver = getDriver();
+    }
+
+    @AfterTest
+    public void closeDriver() {
+        quitDriver();
+    }
+}
---
+++ src/main/java/driver/DriverCreation.java
@@ -0,0 +1,33 @@
+package driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+/*
+    Patten Singleton
+*/
+public class DriverCreation {
+
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    public static WebDriver getDriver() {
+        if (driver.get() == null) {
+            WebDriverManager.chromedriver().setup();
+            WebDriver webDriver = new ChromeDriver();
+            webDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+            webDriver.manage().window().maximize();
+            driver.set(webDriver);
+        }
+        return driver.get();
+    }
+
+    public static void quitDriver() {
+        if (driver.get() != null) {
+            driver.get().close();
+            driver.get().quit();
+            driver.remove();
+        }
+    }
+
+}
