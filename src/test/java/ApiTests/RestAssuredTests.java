d9f18d23d5ba122a4b7409ebab250bbbfc6e82b5
Dzmitry <bogdanov111222@yandex.ru>
2021-11-13 18:02:03 +0300

    added tests for logout and check that user is logedout

--- src/test/java/ApiTests/RestAssuredTests.java
+++ src/test/java/ApiTests/RestAssuredTests.java
@@ -5,7 +5,6 @@ import com.google.gson.JsonObject;
 import io.restassured.response.Response;
 import lombok.extern.log4j.Log4j2;
 import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -110,6 +109,38 @@ public class RestAssuredTests {
 
         log.info("Сейчас в " + jsonObject.get("city") + " " + jsonObject.getAsJsonObject("now").get("phenomena")
                 + "; температура воздуха = " + jsonObject.getAsJsonObject("now").get("temperature") + " градуса");
+
+    }
+    @Test(priority = 3)
+    public void logoutUser() {
+
+        baseURI = "https://www.onliner.by";
+        String endpoint = "/sdapi/user.api/logout";
+        String requestBody = readFileAsString("getToken");
+
+        Response response = given().header("Content-Type", "application/json")
+                .and().body(requestBody)
+                .and().post(endpoint);
+
+        Assert.assertEquals(response.statusCode(), 200);
+        log.info("User is deleted");
+
+    }
+    @Test(priority = 4)
+    public void checkUserIsLogedOut() {
+
+        baseURI = "https://www.onliner.by";
+        String endpoint = "/sdapi/user.api/me";
+
+        Response response = given().header("Authorization", "Bearer " + token)
+                .get(endpoint)
+                .then().extract().response();
+
+        JsonObject jsonObject = gson.fromJson(response.asPrettyString(), JsonObject.class);
+        Assert.assertEquals(response.statusCode(), 401);
+
+        log.info("Status Code is " + response.statusCode() + " Unauthorized");
+        Assert.assertEquals(jsonObject.get("message"), "Bad Credentials");
 
     }
 
