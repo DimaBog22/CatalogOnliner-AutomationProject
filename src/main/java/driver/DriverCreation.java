8adcfe31fb5855369e8ee17c03dcc85854977d5f
Dzmitry <bogdanov111222@yandex.ru>
2021-11-15 19:58:47 +0300

    Update DriverCreation.java

--- src/main/java/driver/DriverCreation.java
+++ src/main/java/driver/DriverCreation.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 /*
-    Patten Singleton
+    Pattern Singleton
 */
 public class DriverCreation {
 
